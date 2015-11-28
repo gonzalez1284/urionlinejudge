73f10a9c27f44a6b0ba9ffd6cf7d24d07b5ae0c3
gonzalez1284 <gonzalez1284@gmail.com>
2015-11-28 21:01:10 -0200

    Create 1016.c

---
+++ 1016.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+  
+int main() {
+  
+    int distancia, x;
+ 
+    scanf("%d", &distancia);
+ 
+    x = (60 * distancia) / 30;
+ 
+    printf("%d minutos\n", x);
+  
+    return 0;
+}
