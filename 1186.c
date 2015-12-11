b98d4015c4fa5c5aed25780bec09fab725ec0e3b
Thiago Costa <gonzalez1284@gmail.com>
2015-12-11 10:28:17 -0200

    Create 1186.c

---
+++ 1186.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int main(){
+
+    int i, j, k;
+    double m[12][12], tmp = 0;
+    char o;
+
+    scanf(" %c", &o);
+
+    for(i = 0; i < 12; i++){
+
+        for(j = 0; j < 12; j++){
+
+            scanf("%lf", &m[i][j]);
+
+        }
+
+    }
+
+    k = 12;
+
+    for(i = 0; i < 12; i++){
+
+        for(j = k; j < 12; j++){
+
+            tmp += m[i][j];
+
+        }
+
+        k--;
+
+
+
+    }
+
+    if(o == 'S'){
+
+        printf("%.1lf\n", tmp);
+
+    } else if(o == 'M'){
+
+        printf("%.1lf\n", tmp / 66.0);
+
+    }
+
+
+    return 0;
+}
